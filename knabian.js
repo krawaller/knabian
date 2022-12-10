df58af006aa03719e2dacf743e933821e915e033
David <david@krawaller.se>
2022-12-10 08:04:00 +0100

    Fabbs

--- knabian.js
+++ knabian.js
@@ -1,11 +1,16 @@
+const marquee = document.getElementById("marquee");
 const video = document.querySelector("video");
-const fireworks = new Fireworks(
-  document.querySelector("#fireworks-container"),
-  {}
-);
+const fabbs = document.getElementById("fabbs");
+let fireworks;
+
+const startParty = (e) => {
+  e.stopPropagation();
+  document.body.classList.add("party-started");
+  fireworks = new Fireworks(document.querySelector("#fireworks-container"), {});
+  toggleParty();
+};
 
 const toggleParty = () => {
-  document.body.classList.add("party-started");
   if (video.paused) {
     fireworks.start();
     video.play();
@@ -15,6 +20,31 @@ const toggleParty = () => {
   }
 };
 
-document.getElementById("start-party-button", toggleParty);
+document
+  .getElementById("start-party-button")
+  .addEventListener("click", startParty);
 
 document.body.addEventListener("click", toggleParty);
+
+const marqueeLines = [
+  "Vem är bäst av ALLA?",
+  "Who you gonna call?",
+  "Säg vem som snyggast i landet är?",
+  "Vem är störst och vackrast i skogen?",
+];
+const updateMarquee = () => {
+  const currentLine = marquee.innerText;
+  const currentIdx = marqueeLines.findIndex((l) => l === currentLine);
+  const nextIdx = currentIdx === marqueeLines.length - 1 ? 0 : currentIdx + 1;
+  marquee.innerText = marqueeLines[nextIdx];
+  fabbs.classList.add("flash");
+  setTimeout(() => {
+    fabbs.classList.remove("flash");
+  }, 500);
+};
+
+const setMarquee = marquee.addEventListener("animationiteration", () => {
+  updateMarquee();
+});
+
+updateMarquee();
